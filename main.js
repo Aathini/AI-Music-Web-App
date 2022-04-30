54faf667e0dc57ea89527e57696f001b614f5f5a
Aathini <78332561+Aathini@users.noreply.github.com>
2022-04-29 21:43:37 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+song = "";
+function preload() {
+    song = loadSound('AmongUs.mp3.mp3', 'Enemy.mp3.mp3');
+}
 function setup() {
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
 
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+}
+function start() {
+    song.play();
 }
