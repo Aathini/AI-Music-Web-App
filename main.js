db49c4ffcdfbaa53bd18c320c3dfa90a42f2aa23
Aathini <78332561+Aathini@users.noreply.github.com>
2022-05-11 23:07:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload() {
     song = loadSound('AmongUs.mp3.mp3', 'Enemy.mp3.mp3');
 }
@@ -9,6 +13,22 @@ function setup() {
     video.size(300,300);
     video.hide();
 
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Model has been loaded");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        scoreLW = results[0].pose.keypoints[9].score;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX + "leftWristY =" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
 function draw() {
     image(video, 0, 0, 300, 300);
